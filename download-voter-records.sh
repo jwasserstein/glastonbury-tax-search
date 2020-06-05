c4d50ddf8e8bdd98ae0f5a30e3964c28f0615081
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-05 13:42:38 -0400

    Refactored download-voter-records.sh to use array concatenation instead of string concatenation for the sake of speed.

--- download-voter-records.sh
+++ download-voter-records.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash 
 
 cd voter-records
+j=()
 for i in {1..2653}; do
-	j="$j\nhttps://voterrecords.com/voters/glastonbury-ct/$i"
+	j+=("https://voterrecords.com/voters/glastonbury-ct/$i\n")
 done
-echo -e $j | xargs -n 1 -P 10 wget 
+echo -e ${j[@]} | xargs -n 1 -P 10 wget -q
