da48ebf19b8830257ebbc28d1826ca1e25679d58
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-05 13:01:46 -0400

    Refactored download-records.sh to store urls in a variable instead of using urls.txt.  Removed urls.txt.  Added *.swp to .gitignore

--- download-records.sh
+++ download-records.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
-
-rm urls.txt
-for j in {1..2653}; do
-	echo "https://voterrecords.com/voters/glastonbury-ct/$j" >> urls.txt
-done
+#!/usr/bin/env bash 
 
 cd voter-records
-cat ../urls.txt | xargs -n 1 -P 10 wget -q
+for i in {1..3}; do
+	j="$j\nhttps://voterrecords.com/voters/glastonbury-ct/$i"
+done
+echo -e $j | xargs -n 1 -P 10 wget 
