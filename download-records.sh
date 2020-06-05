5552e778cc160ae8d40acd77ef48d9bd61bb96b9
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-05 12:24:50 -0400

    Modified download-records.sh to work from higher level directory.  Wrote first working copy of parse-records.py.  Added output files urls.txt, names.txt, and formatted-names.txt

--- download-records.sh
+++ download-records.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-cd voter-records
-
 rm urls.txt
 for j in {1..2653}; do
 	echo "https://voterrecords.com/voters/glastonbury-ct/$j" >> urls.txt
 done
 
-cat urls.txt | xargs -n 1 -P 10 wget -q
+cd voter-records
+cat ../urls.txt | xargs -n 1 -P 10 wget -q
