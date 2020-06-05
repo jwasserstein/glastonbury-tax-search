49a15f97ba69d6fc7ceda1ccee5e9fa23875382c
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-05 13:05:52 -0400

    Renamed download-records.sh to download-voter-records.sh.  Restored loop from 3 iterations to 2653 iterations.

--- download-voter-records.sh
+++ download-voter-records.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 cd voter-records
-for i in {1..3}; do
+for i in {1..2653}; do
 	j="$j\nhttps://voterrecords.com/voters/glastonbury-ct/$i"
 done
 echo -e $j | xargs -n 1 -P 10 wget 
