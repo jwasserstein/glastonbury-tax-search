5269a998963f8420af1ce5ac3d7731dabddd493c
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-06 15:19:44 -0400

    Refactored download-voter-records.sh to use printf instead of a loop to generate URLs to download.

--- download-voter-records.sh
+++ download-voter-records.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash 
 
 cd voter-records
-j=()
-for i in {1..2653}; do
-	j+=("https://voterrecords.com/voters/glastonbury-ct/$i\n")
-done
-echo -e ${j[@]} | xargs -n 1 -P 10 wget -q
+printf "https://voterrecords.com/voters/glastonbury-ct/%s\\n" {1..2653} | xargs -n 1 -P 10 wget -q
