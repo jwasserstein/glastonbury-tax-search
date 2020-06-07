ca08ac61d6a049c4d82219822d21eb65e397bccc
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-07 11:25:24 -0400

    Updated download-voter-records.sh to download records from south glastonbury.

--- download-voter-records.sh
+++ download-voter-records.sh
@@ -2,3 +2,4 @@
 
 cd voter-records
 seq 1 2653 | xargs -I {} -n 1 -P 10 wget https://voterrecords.com/voters/glastonbury-ct/{}
+seq 1 497 | xargs -I {} -n 1 -P 10 wget https://voterrecords.com/voters/south+glastonbury-ct/{}
