7b97d785b34075752e6e5a38543db8c5b58b28f0
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-06 21:58:58 -0400

    Fixed bug with apostrophes in names.

--- download-tax-records.sh
+++ download-tax-records.sh
@@ -6,9 +6,10 @@ post_request2='&searchbtn=Search&propertyNumber=&propertyName=&billYear=&billTyp
 j=()
 while read name; do
 	j+=($name)
-done < formatted-names.txt
+#done < formatted-names.txt
+done < <(sort <(ls tax-records) <(ls tax-records) formatted-names.txt | uniq -u | sed -E "s/'//")
 
 cd tax-records
 wget -q --keep-session-cookies --save-cookies cookies.txt --delete-after "https://www.mytaxbill.org/inet/bill/home.do?town=glastonbury"
-echo -e ${j[@]:1:30} | xargs -n 1 -I{} -P 10 wget -q -O "{}" --load-cookies cookies.txt --post-data "$post_request1{}$post_request2" https://www.mytaxbill.org/inet/bill/search.do
+echo -e ${j[@]} | xargs -n 1 -I{} -P 10 wget -q -O "{}" --load-cookies cookies.txt --post-data "$post_request1{}$post_request2" https://www.mytaxbill.org/inet/bill/search.do
 rm cookies.txt
