598cafb526694c528c15606f12f47fc6d42b8d40
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-09 21:10:05 -0400

    Added parse-street-names.py and streets.txt.  Sorted vehicles.txt.

---
+++ parse-street-names.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import os
+import re
+
+def grab_street_name(html):
+	s = BeautifulSoup(html, 'html.parser')
+	a = s.find_all('a', {'href': re.compile('Streets.aspx\?Name=[A-Z]*')})	
+	ret = []
+	for i in a:
+		ret.append(i.get_text())
+	return ret
+
+if __name__ == '__main__':
+	streets = []
+	files = os.listdir('street-records')
+	numfiles = len(files)
+	count = 0
+	for f in files:
+		count += 1
+		print('parsing file {} of {}.  File name: {}'.format(count, numfiles, f))
+		with open('street-records/{}'.format(f), 'r') as rfh:
+			streets += grab_street_name(rfh.read())
+	with open('streets.txt', 'w') as wfh:
+		for i in range(0, len(streets)):
+			wfh.write('{}\n'.format(streets[i]))
