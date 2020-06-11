b9029d6b4a0b14bcc60e1f5896992fd564180855
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-10 22:59:54 -0400

    Modified download-voter-records-street.py to fix bugs and only download missing files.  Added names-street.txt and updated .gitignore to ignore log files.

--- download-voter-records-street.py
+++ download-voter-records-street.py
@@ -2,26 +2,45 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import re
+import os
+
+delay = 30  # delay between downloads in seconds
+
+
+def save_html(html, name, number):
+	print('Writing file {}-{}.html'.format(name, number))
+	with open('voter-records-street/{}-{}.html'.format(name, number), 'w') as wfh:
+		wfh.write(html)	
+
 
 if __name__ == '__main__':
 	with open('streets.txt', 'r') as rfh:
-		streets = rfh.read().split('\n')[:-1]
-	for i in streets:
+		streets = set(rfh.read().split('\n')[:-1])  # determine which files are needed
+
+	ls = os.listdir('voter-records-street')
+	r = re.compile('^(.*)-[0-9]+.?.*$')
+	existing_files = []
+	for j in ls:
+		m = r.match(j)
+		existing_files.append(m.group(1))  # determine which files already exist
+	existing_files = set(existing_files)
+
+	needed_files = streets - existing_files  # determine which files must be downloaded
+	print('{} of {} streets still need to be downloaded'.format(len(needed_files), len(streets)))
+	for i in needed_files:
 		print('Attempting to fetch {}'.format(i))
 		url = 'https://voterrecords.com/street/{}-glastonbury-ct/'.format(i)
 		response = requests.get(url)	
 		if response.status_code != 200:
 			print("Glastonbury didn't work, trying South Glastonbury")
-			time.sleep(20)
+			time.sleep(delay)
 			url = 'https://voterrecords.com/street/{}-south+glastonbury-ct/'.format(i)
 			response = requests.get(url)
 			if response.status_code != 200:
 				print("Couldn't fetch {}".format(i))
 				continue
 
-		print('Writing file {}-{}'.format(i, 1))
-		with open('voter-records-street/{}-{}'.format(i, 1), 'w') as wfh:
-			wfh.write(response.text)	
+		save_html(response.text, i, 1)
 
 		s = BeautifulSoup(response.text, 'html.parser')
 		p = s.find('label', {'class': 'lead BottomMarginZero pull-left'})
@@ -29,11 +48,10 @@ if __name__ == '__main__':
 		if o is None:
 			print("couldn't find page numbers")
 			continue
-		for j in range(2, int(o.group(1))):  # start at 2 because first page was already written
-			time.sleep(20)
+		print('found {} pages'.format(o.group(1)))
+		for j in range(2, int(o.group(1)) + 1):  # start at 2 because first page was already written
+			time.sleep(delay)
 			page_url = url + str(j)
 			response = requests.get(page_url)
-			print('Writing file {}-{}'.format(i, j))
-			with open('voter-records-street/{}-{}'.format(i, j), 'w') as wfh:
-				wfh.write(response.text)	
-		time.sleep(20)
+			save_html(response.text, i, j)
+		time.sleep(delay)
