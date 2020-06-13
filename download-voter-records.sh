19ad97d355b71ac97cae5440b400ddeca8c74478
justinwasserstein <justin.wasserstein@gmail.com>
2020-06-12 21:29:29 -0400

    Removed non-street based scripts.

--- download-voter-records.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash 
-
-cd voter-records
-seq 1 2653 | xargs -I {} -n 1 -P 10 wget https://voterrecords.com/voters/glastonbury-ct/{}
-seq 1 497 | xargs -I {} -n 1 -P 10 wget https://voterrecords.com/voters/south+glastonbury-ct/{}
--- format-names.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cat names.txt | awk '{if($3==""){print $2"+"$1}else{print $3"+"$1}}' | sort | uniq | sed -E "s/'//"
